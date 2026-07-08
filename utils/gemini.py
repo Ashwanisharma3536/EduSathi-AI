aa42bab03fe29fd21e10c2d4e8557d38dff905ec
Ashwani Sharma <ashwanisharma3536@gmail.com>
2026-07-08 06:13:17 +0530

    Fix Gemini API

--- utils/gemini.py
+++ utils/gemini.py
@@ -1,10 +1,19 @@
 import os
+import streamlit as st
 from dotenv import load_dotenv
 from google import genai
 
 load_dotenv()
 
-client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+# Pehle .env se key lo
+API_KEY = os.getenv("GEMINI_API_KEY")
+
+# Agar .env me nahi mili to Streamlit Secrets se lo
+if not API_KEY:
+    API_KEY = st.secrets["GEMINI_API_KEY"]
+
+# Client banao
+client = genai.Client(api_key=API_KEY)
 
 def ask_gemini(prompt):
     response = client.models.generate_content(
